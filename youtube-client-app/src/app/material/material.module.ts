e4f4488867afb44acc90df6238a72eb5a9e18dfc
tatafox <pavlovetstatyana@gmail.com>
2022-03-25 10:37:24 +0300

    refractor: added css variables and refractor theme

--- youtube-client-app/src/app/material/material.module.ts
+++ youtube-client-app/src/app/material/material.module.ts
@@ -4,12 +4,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 const modules = [
   MatInputModule,
   MatFormFieldModule,
   MatButtonModule,
   MatIconModule,
+  MatButtonToggleModule,
 ];
 
 @NgModule({
