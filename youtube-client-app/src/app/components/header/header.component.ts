1bfd835ca1c9e6f1d76631595f4c32af10f2d7c9
tatafox <pavlovetstatyana@gmail.com>
2022-03-27 12:34:27 +0300

    feat: added Filtering criteria block toggle

--- youtube-client-app/src/app/components/header/header.component.ts
+++ youtube-client-app/src/app/components/header/header.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() { }
+  settingActive = false;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 }
