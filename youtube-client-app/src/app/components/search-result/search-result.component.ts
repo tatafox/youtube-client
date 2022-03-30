a3cf84c8364019f13be25415263842cd369f6174
tatafox <pavlovetstatyana@gmail.com>
2022-03-30 12:46:08 +0300

    refactor: apply eslint

--- youtube-client-app/src/app/components/search-result/search-result.component.ts
+++ youtube-client-app/src/app/components/search-result/search-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ISearchItem } from '../../common/models/search-items.models';
 
 @Component({
