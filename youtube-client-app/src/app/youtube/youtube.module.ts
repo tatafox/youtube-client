6fae0da107c21f2eb5f7ca3e8128386518aa0b9f
tatafox <pavlovetstatyana@gmail.com>
2022-04-07 21:28:39 +0300

    feat: added details info card

--- youtube-client-app/src/app/youtube/youtube.module.ts
+++ youtube-client-app/src/app/youtube/youtube.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SearchResultComponent } from './components/search-result/search-result.component';
 import { SearchItemComponent } from './components/search-result/search-item/search-item.component';
+import { DetailInfoComponent } from './components/detail-info/detail-info.component';
 
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
+import { StatisticComponent } from './components/statistic/statistic.component';
 
 @NgModule({
   declarations: [SearchResultComponent,
-    SearchItemComponent],
+    SearchItemComponent,
+    DetailInfoComponent,
+    StatisticComponent],
   imports: [
     CommonModule,
     MaterialModule,
@@ -18,6 +22,7 @@ import { SharedModule } from '../shared/shared.module';
   exports: [
     SearchResultComponent,
     SearchItemComponent,
+    DetailInfoComponent,
   ],
 })
 export class YoutubeModule { }
